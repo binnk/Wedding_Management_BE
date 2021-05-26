660cdf9975095e926a60b113742dab3420ee6e68
binnk <68799995+binnk@users.noreply.github.com>
2021-05-26 18:37:18 +0700

    Update LobbyAPI.java

--- wedding/src/main/java/home/javaweb/api/LobbyAPI.java
+++ wedding/src/main/java/home/javaweb/api/LobbyAPI.java
@@ -9,15 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import home.javaweb.entity.LobbyEntity;
 import home.javaweb.service.ILobbyService;
-import home.javaweb.service.IShiftService;
-@RequestMapping("/api")
-@RestController
+
 @CrossOrigin
+@RestController
+@RequestMapping("/api")
 public class LobbyAPI {
    @Autowired
    private ILobbyService lobbySer;
 
-   @GetMapping(path = "/lobby")
+   @GetMapping("/lobby")
 	public List<LobbyEntity> weddingPage() {
 		return lobbySer.findAll();
 	}
