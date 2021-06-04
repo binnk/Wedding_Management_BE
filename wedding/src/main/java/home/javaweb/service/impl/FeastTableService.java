cde3f792f12cb69e2e22567454e308213301163e
binnk <68799995+binnk@users.noreply.github.com>
2021-06-04 21:55:09 +0700

    Check minTablePrice(Lobby) vs UnitPrice (FeastTable)

--- wedding/src/main/java/home/javaweb/service/impl/FeastTableService.java
+++ wedding/src/main/java/home/javaweb/service/impl/FeastTableService.java
@@ -1,6 +1,5 @@
 package home.javaweb.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +7,10 @@ import org.springframework.stereotype.Service;
 
 import home.javaweb.converter.FeastTableConverter;
 import home.javaweb.dto.FeastTableDTO;
+import home.javaweb.entity.FeastEntity;
 import home.javaweb.entity.FeastTable;
 import home.javaweb.repository.FeastTableRepository;
+import home.javaweb.service.IFeastService;
 import home.javaweb.service.IFeastTableService;
 
 
@@ -18,24 +19,29 @@ public class FeastTableService implements IFeastTableService {
 	
 	@Autowired
 	private FeastTableRepository _repository;
+	@Autowired
+	private IFeastService _feastService;
 	
 	@Autowired
 	private FeastTableConverter _converter;
-
 
 
 	@Override
 	public FeastTableDTO save(FeastTableDTO dto) {
 		
 		FeastTable record = _converter.toEntity(dto);
-		_repository.save(record);
 		
+		FeastEntity feastEntity = _feastService.findById(dto.getFeastId());
+		Long minPriceTable = feastEntity.getId_lobby().getMin_unitpricetable();
+
+		if(minPriceTable > record.getUnitPriceTable())
+			record.setUnitPriceTable(minPriceTable);
+		
+		_repository.save(record);
 		FeastTableDTO result = findByFeastId(dto.getFeastId());
 		
 		return result;
 	}
-
-
 
 	@Override
 	public FeastTableDTO findByFeastId(Long feastId) {
@@ -48,25 +54,25 @@ public class FeastTableService implements IFeastTableService {
 		
 	}
 
-
-
 	@Override
 	public void deleteById(Long[] ids) {
 		for(Long id : ids)
 			_repository.deleteById(id);		
 	}
 
-
-
 	@Override
 	public FeastTable findById(Long id) {
 		return _repository.findById(id).get();
 	}
 
-
-
 	@Override
 	public FeastTable save(FeastTable entity) {
+		FeastEntity feastEntity = _feastService.findById(entity.getFeast().getId());
+		Long minPriceTable = feastEntity.getId_lobby().getMin_unitpricetable();
+
+		if(minPriceTable > entity.getUnitPriceTable())
+			entity.setUnitPriceTable(minPriceTable);
+		
 		return _repository.save(entity);
 	}
 
