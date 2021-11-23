52af45f44214cec3389cbfac64a352607fa9a6c1
Phạm Tấn khang <phamtankhang2001@gmail.com>
2021-11-23 20:58:34 +0700

    Update DepositConfigAPI.java

--- wedding/src/main/java/home/javaweb/api/DepositConfigAPI.java
+++ wedding/src/main/java/home/javaweb/api/DepositConfigAPI.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,8 @@ public class DepositConfigAPI {
 	}
 	
 	@PutMapping
-	public ResponseEntity<Object> changeDepositConfig(DepositConfig deposit) {
+	public ResponseEntity<Object> changeDepositConfig(@RequestBody DepositConfig deposit) {
+		deposit.setId(1L);
 		DepositConfig newDeposit = repository.save(deposit)	;
 		return new ResponseEntity<>(deposit,HttpStatus.OK);
 	}
